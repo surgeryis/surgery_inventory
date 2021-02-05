ae19e13eb92022807b4843ef4c0f41783745b398
TimMitzen <45426633+TimMitzen@users.noreply.github.com>
2021-02-05 15:01:59 -0500

    Update settings.py

--- work_inventory/settings.py
+++ work_inventory/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Inventory',
         'USER': 'postgres',
-        'PASSWORD': 'tim',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432'
     }
